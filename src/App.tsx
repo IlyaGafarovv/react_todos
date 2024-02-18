07163f9b0737829dfcc69799c3bff422873e18de
IlyaGafarovv <ilyagafarov@bk.ru>
2024-02-18 19:15:33 +0600

    initial styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import React from 'react'
 import './App.css'
 
+import { Todo } from './types/Todo'
+
+import TodoForm from './components/Todos/TodoForm'
+import TodoList from './components/Todos/TodoList'
+
 export default function App() {
-  return <h1>Todos App</h1>
+  const [todos, setTodos] = React.useState<Todo[]>([])
+
+  return (
+    <div className='app'>
+      <div className='gradient' />
+
+      <main className='main'>
+        <h1 className='title'>
+          todos <span>app</span>
+        </h1>
+        <TodoForm />
+        <TodoList />
+      </main>
+    </div>
+  )
 }
